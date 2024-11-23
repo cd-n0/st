c9e0af21bd3a9ae0ed67f7ea9afc391a62a411c7
cdn0 <59338209+cd-n0@users.noreply.github.com>
2024-11-23 12:24:16 +0300

    scrollback patch

--- st.c
+++ st.c
@@ -2757,3 +2757,11 @@ redraw(void)
 	tfulldirt();
 	draw();
 }
+
+void historyQuit() { historyModeToggle(0); }
+
+void historyShiftY(Arg const *y) {
+	if (!histMode) historyModeToggle(1);
+	historyMove(0, 0, y->i);
+	if (insertOff == histOff) historyModeToggle(0);
+}
