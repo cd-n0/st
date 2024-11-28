c96a7f5ed565769791ff5afa66b116040f051198
cdn0 <59338209+cd-n0@users.noreply.github.com>
2024-11-28 16:45:52 +0300

    I don't know why xresources patch doesn't include this

--- config.def.h
+++ config.def.h
@@ -118,10 +118,10 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
+	"black", /* default background colour */
+	"gray90", /* default foreground colour */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
 };
 
 
@@ -129,9 +129,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 unsigned int const buffSize = 255;
 
